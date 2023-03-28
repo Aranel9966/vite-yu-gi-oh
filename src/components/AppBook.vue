0acd0ac84cf45ee33d9bb0d103255501b04061eb
Aranel9966 <99angelo.dire@gmail.com>
2023-03-28 19:00:34 +0200

    add v-show

--- src/components/AppBook.vue
+++ src/components/AppBook.vue
@@ -5,7 +5,8 @@ export default{
     name:'AppBook',
     data(){
         return{
-            
+            search:'',
+            searchType:'',
             store,
         }
     },
@@ -14,20 +15,37 @@ export default{
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res)=>{
             console.log(res.data.data);
             this.store.cards = res.data.data;
+            console.log(this.store.cards[13].frameType);
         })
+    },
+    methods:{
+        
     }
 }
 </script>
 <template>
+    <div class="top">
+        <h1>YU-GI-OH</h1>
+        <input v-model="search" type="text" placeholder="Cerca per nome">
+        
+    </div>
     <div class="card-list">
 
-        <div v-for="card in store.cards" class="card">
+        <div v-show="card.name.toLowerCase().includes(this.search.toLowerCase()) " v-if="store.cards.length"  v-for="card in store.cards" class="card">
             <img :src="card.card_images[0].image_url" alt="">
+        </div>
+        <div v-else>
+            <h1>
+                Loading...
+            </h1>
         </div>
                 
     </div>
     </template>
 <style>
+    .top{
+        text-align: center;
+    }
     .card-list{
         display: flex;
         flex-flow: row wrap;
